152cff08ff0ffe00a98113aab6faf1013fd5ed03
Pavlos Vinieratos <pvinis@gmail.com>
2020-03-06 09:48:57 +0400

    try again

--- index.js
+++ index.js
@@ -4,30 +4,36 @@ const github = require('@actions/github')
 
 const run = async () => {
 	try {
+		console.debug('start')
 		// if (github.context.payload.action !== "kati") return
 
+		console.debug('get issue')
 		const issue = github.context.payload.issue
 		if (!issue) {
 			console.error('no issue')
 			return
 		}
 
-		const token = process.env['GITHUB_TOKEN']
+		console.debug('get token')
+		const token = core.getInput('github-token')
 		if (!token) {
 			console.error('no token')
 			return
 		}
 
+		console.debug('init github')
 		const octokit = new github.GitHub(token)
 		const nwo = process.env['GITHUB_REPOSITORY'] || '/'
 		const [owner, repo] = nwo.split('/')
 
+		console.debug('send message')
 		const happyBirthdayMessage = 'Happy Birthday!!'
 		const issueCommentResponse = await octokit.issues.createComment({
 			owner, repo, issue_number: issue.number,
 			body: happyBirthdayMessage,
 		})
 		console.log('wrote happy borthday')
+		console.debug('end')
 	} catch (error) {
 		console.error(error.message)
 		core.setFailed(`happy-birthday action failure: ${error}`)
